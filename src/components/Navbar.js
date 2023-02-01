645dca3c71f4046f506c8c947172ef04f927623f
Pitching <adchir95@gmail.com>
2023-02-01 14:01:28 -0500

    adding react bootstrap icons to project

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,37 +8,26 @@ function Navbar() {
           <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
             <a href="/" class="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
               <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlinkHref="#bootstrap" /></svg>
+              Andrei Chirita
             </a>
 
             <ul class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
               <li>
-                <a href="#" class="nav-link text-secondary">
+                <a href="#" class="nav-link text-white">
                   <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlinkHref="#home" /></svg>
-                  Home
+                  About
                 </a>
               </li>
               <li>
                 <a href="#" class="nav-link text-white">
                   <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlinkHref="#speedometer2" /></svg>
-                  Dashboard
+                  Resume
                 </a>
               </li>
               <li>
                 <a href="#" class="nav-link text-white">
                   <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlinkHref="#table" /></svg>
-                  Orders
-                </a>
-              </li>
-              <li>
-                <a href="#" class="nav-link text-white">
-                  <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlinkHref="#grid" /></svg>
-                  Products
-                </a>
-              </li>
-              <li>
-                <a href="#" class="nav-link text-white">
-                  <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlinkHref="#people-circle" /></svg>
-                  Customers
+                  Projects
                 </a>
               </li>
             </ul>
