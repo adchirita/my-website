f0c1908c8f4f94446e2a5b5c25f559a08fcc2aa3
Pitching <adchir95@gmail.com>
2023-02-04 16:25:14 -0500

    adding stylizing and including a main cover page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.scss';
 import Navbar from './components/Navbar';
+import MainCover from './components/MainCover';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <MainCover />
     </div>
   );
 }
