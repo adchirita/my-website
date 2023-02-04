b43978fc4f7a30919ec6157483eb2fa94f30e24b
Pitching <adchir95@gmail.com>
2023-02-04 16:40:38 -0500

    removing unnecessary code from navbar

--- src/components/MainCover.js
+++ src/components/MainCover.js
@@ -2,11 +2,11 @@ import './MainCover.scss';
 
 function MainCover() {
   return (
-    <main class="px-3 text-center text-bg-dark MainWrapper">
+    <main class="px-3 text-center row text-bg-dark MainWrapper">
       <h1>Cover your page.</h1>
       <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
       <p class="lead">
-        <a href="#" class="btn btn-lg btn-light fw-bold border-white bg-white">Learn more</a>
+        <a href="#" class="btn btn-lg btn-light fw-bold border-white bg-white">Learn more about me</a>
       </p>
     </main>
   )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,17 +4,17 @@ import { CollectionFill, FileEarmarkTextFill, FilePerson } from 'react-bootstrap
 function Navbar() {
   return (
     <header>
-      <div class="px-3 py-2 text-bg-dark">
+      <div class="py-2 text-bg-dark">
         <div class="container">
           <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-            <a href="/" class="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
+            <a href="/" class="me-lg-auto text-white text-decoration-none">
               <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
 
               </svg>
               Andrei Chirita
             </a>
 
-            <ul class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0">
+            <ul class="nav">
               <li>
                 <a href="#" class="nav-link text-white">
                   <svg class="bi d-block mx-auto mb-1" width="24" height="24">
