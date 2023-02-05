bf52ae0f5353cbfd049b5928389fa5131213153d
Pitching <adchir95@gmail.com>
2023-02-05 16:24:24 -0500

    fixing the margin issues and adding a title to the tech stack component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,15 @@
 import Description from "./Description"
 import TechStack from "./TechStack";
+import { Link } from "react-router-dom";
 
 function About() {
   return (
     <div>
       <Description />
       <TechStack />
+      <div class="text-center">
+      <Link to="/projects" class="mb-4 btn btn-lg btn-light fw-bold border-white bg-white">Check out some of my projects</Link>
+      </div>
     </div>
   )
 }
--- src/components/About/TechStack.js
+++ src/components/About/TechStack.js
@@ -13,35 +13,38 @@ function TechStack() {
 
 
   return (
-    <div class="bg-secondary mx-auto rounded-5 py-3 d-grid gap-3 TechContainer">
-      <div class="d-flex flex-row justify-content-between py-3">
-        {<img src={images['javascript.svg']} />}
-        {<img src={images['bootstrap.svg']} />}
-        {<img src={images['css-3.svg']} />}
-        {<img src={images['express.svg']} />}
+    <main>
+      <h1 class="display-2 text-white text-center"><strong><u>Tech Stack</u></strong></h1>
+      <div class="bg-secondary mx-auto rounded-5 py-3 d-grid gap-3 TechContainer">
+        <div class="d-flex flex-row justify-content-between py-3">
+          {<img src={images['javascript.svg']} />}
+          {<img src={images['bootstrap.svg']} />}
+          {<img src={images['css-3.svg']} />}
+          {<img src={images['express.svg']} />}
+        </div>
+        <div class="d-flex flex-row justify-content-between py-3">
+          {<img src={images['git-icon.svg']} />}
+          {<img src={images['html-5.svg']} />}
+          {<img src={images['leaflet.svg']} />}
+          {<img src={images['git-icon.svg']} />}
+        </div>
+        <div class="d-flex flex-row justify-content-between py-3">
+          {<img src={images['mocha.svg']} />}
+          {<img src={images['nodejs-icon.svg']} />}
+          {<img src={images['postgresql.svg']} />}
+          {<img src={images['python.svg']} />}
+        </div>
+        <div class="d-flex flex-row justify-content-between py-3">
+          {<img src={images['rails.svg']} />}
+          {<img src={images['react.svg']} />}
+          {<img src={images['sass.svg']} />}
+          {<img src={images['selenium.svg']} />}
+        </div>
+        <div class="d-flex flex-row justify-content-center py-3">
+          {<img src={images['storybook-icon.svg']} />}
+        </div>
       </div>
-      <div class="d-flex flex-row justify-content-between py-3">
-        {<img src={images['git-icon.svg']} />}
-        {<img src={images['html-5.svg']} />}
-        {<img src={images['leaflet.svg']} />}
-        {<img src={images['git-icon.svg']} />}
-      </div>
-      <div class="d-flex flex-row justify-content-between py-3">
-        {<img src={images['mocha.svg']} />}
-        {<img src={images['nodejs-icon.svg']} />}
-        {<img src={images['postgresql.svg']} />}
-        {<img src={images['python.svg']} />}
-      </div>
-      <div class="d-flex flex-row justify-content-between py-3">
-        {<img src={images['rails.svg']} />}
-        {<img src={images['react.svg']} />}
-        {<img src={images['sass.svg']} />}
-        {<img src={images['selenium.svg']} />}
-      </div>
-      <div class="d-flex flex-row justify-content-center py-3">
-        {<img src={images['storybook-icon.svg']} />}
-      </div>
-    </div>
+    </main>
   )
 }
 
