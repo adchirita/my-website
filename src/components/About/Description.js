7e0a7363113a8338fba8423485c4db0da68beb02
Pitching <adchir95@gmail.com>
2023-08-15 12:43:09 -0400

    changing typos

--- src/components/About/Description.js
+++ src/components/About/Description.js
@@ -7,11 +7,11 @@ function Description() {
         <h1 className="display-2"><strong className="fadeIn" id="first">About</strong></h1>
         <br />
         <p className="lead text-start fadeIn mobile-text" id="second">I am a motivated full-stack web developer with a passion for programming and computer software & hardware.
-          As a former QA Analyst, I have developed a strong background in software testing and was exposed to automation
+          As a former SDET, I have developed a strong background in software testing and was exposed to automation
           testing using python and selenium. I worked closely with Bell professionals and developers in agile and waterfall
-          methodologies, and have become very familiar with the software development lifecycle. <br/><br/>Served as a Sr. QA Analyst
+          methodologies, and have become very familiar with the software development lifecycle. <br/><br/>Served as a Sr. QA Engineer
           to successfully deliver one of Bell's top 3 projects in 2020, in preparation for their new devices and modems to be
-          linked with their Maximo database. I aim to take what I have learned as a QA Analyst and apply that to my software
+          linked with their Maximo database. I aim to take what I have learned as a SDET and apply that to my software
           development mentality, finding and stopping bugs before they reach the test environment. Having experienced automation
           testing, I found a true passion for programming and look to find a fulfilling role as a full-stack web developer where
           I can grow my skills and contribute efficiently to something bigger. </p>
